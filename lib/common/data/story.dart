98fd452710cbd4f3d0a3f1264a0efa015610d4da
Ruan Failache <ruanfailache@gmail.com>
2022-09-28 11:47:55 -0300

    fix: choose correct choice

--- lib/common/data/story.dart
+++ lib/common/data/story.dart
@@ -11,22 +11,22 @@ class StoryData {
     final nextStoryOfChoice2 = _currentStory.choice2?.nextStory;
 
     if (choice == 1 && nextStoryOfChoice1 != null) {
-      _currentStory = nextStoryOfChoice1!;
+      _currentStory = nextStoryOfChoice1;
     }
 
     if (choice == 2 && nextStoryOfChoice2 != null) {
-      _currentStory = nextStoryOfChoice1!;
+      _currentStory = nextStoryOfChoice2;
     }
   }
 
   void goBackToInitialStory() {
-    _currentStory = _initialStory!;
+    _currentStory = _initialStory;
   }
 }
 
 final _initialStory = Story(
   title:
-  'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
+      'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
   choice1: Choice(
     title: 'I\'ll hop in. Thanks for the help!',
     nextStory: _story_2,
@@ -51,7 +51,7 @@ final _story_1 = Story(
 
 final _story_2 = Story(
   title:
-  'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
+      'As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box.',
   choice1: Choice(
     title: 'I love Elton John! Hand him the cassette tape.',
     nextStory: _story_5,
@@ -64,7 +64,7 @@ final _story_2 = Story(
 
 final _story_3 = Story(
   title:
-  'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
+      'What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?',
   choice1: Choice(
     title: 'Restart',
   ),
@@ -72,7 +72,7 @@ final _story_3 = Story(
 
 final _story_4 = Story(
   title:
-  'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
+      'As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in.',
   choice1: Choice(
     title: 'Restart',
   ),
@@ -80,7 +80,7 @@ final _story_4 = Story(
 
 final _story_5 = Story(
   title:
-  'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
+      'You bond with the murderer while crooning verses of "Can you feel the love tonight". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: "Try the pier".',
   choice1: Choice(
     title: 'Restart',
   ),
